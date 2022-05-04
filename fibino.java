2251386b7e3098c15298732b63008d751a52ce8c
rajeevkorthiwada <95093433+rajeevkorthiwada@users.noreply.github.com>
2022-05-04 17:25:13 +0530

    Create fibino.java
    program giver the fibonacci series for the given input

---
+++ fibino.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class fibino
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=0,n2=1,n3,count;
+	    count=sc.nextInt();
+		System.out.print(n1+" "+n2);
+		for(int i=2;i<=count;i++){
+		    n3=n1+n2;
+		    System.out.print(" "+n3);
+		    n1=n2;
+		    n2=n3;
+		}
+	}
+}
